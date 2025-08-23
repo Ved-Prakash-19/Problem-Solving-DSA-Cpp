7d3ce0637940e001ed497c936313c0857897ef3c
Ved Prakash <vedprakash811315@gmail.com>
2025-08-23 22:31:51 +0530

    add two numbers

---
+++ 1_basic_cpp_program/2_add_numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, sum;
+    cout << "Enter 1st numbers: ";
+    cin >> a;
+    cout << "Enter 1st numbers: ";
+    cin >> b;   
+    sum = a + b;     
+    cout << "Sum of a + b is = " << sum << endl; 
+    return 0;
+}
+
+
+
+
