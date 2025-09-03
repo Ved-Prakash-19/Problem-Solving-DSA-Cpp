00a18d7552f992a386db6586aec7a2c058761bf6
Ved Prakash <vedprakash811315@gmail.com>
2025-09-03 23:06:41 +0530

    add basic files

--- 1_basic_cpp_program/16_check_perfect_num.cpp
+++ 1_basic_cpp_program/16_check_perfect_num.cpp
@@ -1,33 +1,3 @@
-// #include <iostream>
-// using namespace std;
-
-// int main() {
-//     int num, sum = 0;
-//     cout << "Enter a number: ";
-//     cin >> num;
-
-//     // find sum of divisors
-//     for (int i = 1; i <= num / 2; i++) {
-//         if (num % i == 0) {
-//             sum += i;
-//         }
-//     }
-
-//     // check condition
-//     if (sum == num && num != 0) {
-//         cout << num << " is a Perfect Number." << endl;
-//     } else {
-//         cout << num << " is NOT a Perfect Number." << endl;
-//     }
-
-//     return 0;
-// }
-
-
-
-
-
-
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -60,3 +30,41 @@ int main() {
 
     return 0;
 }
+
+
+
+
+
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int num, sum = 0;
+//     cout << "Enter a number: ";
+//     cin >> num;
+
+//     // find sum of divisors
+//     for (int i = 1; i <= num / 2; i++) {
+//         if (num % i == 0) {
+//             sum += i;
+//         }
+//     }
+
+//     // check condition
+//     if (sum == num && num != 0) {
+//         cout << num << " is a Perfect Number." << endl;
+//     } else {
+//         cout << num << " is NOT a Perfect Number." << endl;
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+
