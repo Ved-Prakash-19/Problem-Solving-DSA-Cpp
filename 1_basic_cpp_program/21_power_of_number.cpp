09606439b01a7d5e52e14e1867e3000dc98da436
Ved Prakash <vedprakash811315@gmail.com>
2025-09-04 23:39:55 +0530

    add basic files

---
+++ 1_basic_cpp_program/21_power_of_number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int base, exponent;
+    long long result = 1;
+
+    cout << "Enter base: ";
+    cin >> base;
+    cout << "Enter exponent: ";
+    cin >> exponent;
+
+    for (int i = 1; i <= exponent; i++) {
+        result *= base;
+    }
+
+    cout << base << "^" << exponent << " = " << result << endl;
+
+    return 0;
+}
