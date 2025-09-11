922bc24ae4484f55cfe21efed5ab9165ab93fee8
Ved Prakash <vedprakash811315@gmail.com>
2025-09-11 23:55:46 +0530

    add basic files

---
+++ 1_basic_cpp_program/25_funcn_without_return_void.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// Function definition
+void greet() {
+    cout << "Hello Gaurav, keep learning C++!" << endl;
+}
+
+int main() {
+    // Function call
+    greet();
+    return 0;
+}
