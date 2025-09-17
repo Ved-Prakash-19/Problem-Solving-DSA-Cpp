9ed331410f6d7689af981c10fac99e330d9c4c2d
Ved Prakash <vedprakash811315@gmail.com>
2025-09-18 03:48:58 +0530

    add basic pattern

--- 2_pattern_printing/5_hollow_square.cpp
+++ 2_pattern_printing/5_hollow_square.cpp
@@ -2,22 +2,17 @@
 using namespace std;
 
 int main(){
-    int length1;
-    int length2;
+    int side;
+    cout<< "Enter side of square :";
+    cin>>side;
 
-    cout<<"Enter number of rows: ";
-    cin>>length1; 
-
-    cout<<"Enter number of cols: ";
-    cin>>length2;
-
-    for(int i=0; i<length1; i++){
-        for(int j=0; j<length2; j++){
-            if(i==0 || i==length1-1){
+    for(int i=0; i<side; i++){
+        for(int j=0; j<side; j++){
+            if(i==0 || i==side-1){
                 cout<<"*\t";
             }
             else{
-                if(j==0 || j==length2-1){
+                if(j==0 || j==side-1){
                     cout<<"*\t";
                 }
                 else{
