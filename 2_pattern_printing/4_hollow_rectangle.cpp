475863dc1db3e173872ed860cbf27f269e905a71
Ved Prakash <vedprakash811315@gmail.com>
2025-09-18 02:21:27 +0530

    add hollow rectangle pattern

---
+++ 2_pattern_printing/4_hollow_rectangle.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int length1;
+    int length2;
+
+    cout<<"Enter number of rows: ";
+    cin>>length1; 
+
+    cout<<"Enter number of cols: ";
+    cin>>length2;
+
+    for(int i=0; i<length1; i++){
+        for(int j=0; j<length2; j++){
+            if(i==0 || i==length1-1){
+                cout<< "* ";
+            }
+            else{
+                if(j==0 || j==length2-1){
+                    cout<< "* ";
+                }
+                else{
+                    cout<< " ";
+                }
+            }
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
