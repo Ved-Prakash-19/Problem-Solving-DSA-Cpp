62ba50a4041ea0688789c82556ed41e1db2c11a3
Ved Prakash <vedprakash811315@gmail.com>
2025-09-18 02:19:08 +0530

    commit basic pattern

---
+++ 2_pattern_printing/1_loop_pattern.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    for (int i = 0; i < 5; i++)
+    {
+        cout << i << "->";
+    
+        for(int j=0; j<=2; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+    
