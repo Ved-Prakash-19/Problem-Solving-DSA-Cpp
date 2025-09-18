e00169030ed2347404ecaaa08a1d3e4ab604f91b
Ved Prakash <vedprakash811315@gmail.com>
2025-09-18 14:11:46 +0530

    add basic pattern files

--- 2_pattern_printing/boundary_pattern.cpp
+++ 2_pattern_printing/boundary_pattern.cpp
@@ -1,38 +1,38 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
 // using functions:-
 
-void printpattern(int n) {
-    int size = 2 * n - 1;
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            int top = i;
-            int left = j;
-            int bottom = size - 1 - i;
-            int right = size - 1 - j;
+// void printpattern(int n) {
+//     int size = 2 * n - 1;
+//     for (int i = 0; i < size; i++) {
+//         for (int j = 0; j < size; j++) {
+//             int top = i;
+//             int left = j;
+//             int bottom = size - 1 - i;
+//             int right = size - 1 - j;
 
-            int layer = min(min(top, bottom), min(left, right));
-            cout << (n - layer) << " ";
-        }
-        cout << endl;
-    }
-}
+//             int layer = min(min(top, bottom), min(left, right));
+//             cout << (n - layer) << " ";
+//         }
+//         cout << endl;
+//     }
+// }
 
-int main() {
-    int t;
-    cout << "Enter number of test cases: ";
-    cin >> t;
+// int main() {
+//     int t;
+//     cout << "Enter number of test cases: ";
+//     cin >> t;
 
-    for (int i = 0; i < t; i++) {
-        int n;
-        cout << "\nEnter value of n: ";
-        cin >> n;
-        printpattern(n);
-    }
+//     for (int i = 0; i < t; i++) {
+//         int n;
+//         cout << "\nEnter value of n: ";
+//         cin >> n;
+//         printpattern(n);
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
 
 
 
