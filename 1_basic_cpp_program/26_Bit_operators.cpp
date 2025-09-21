0f9df54db2be1d14d22ed3730f33a7b93697207c
Ved Prakash <vedprakash811315@gmail.com>
2025-09-22 04:15:05 +0530

    add basic program

---
+++ 1_basic_cpp_program/26_Bit_operators.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<< (5 & 3) <<endl;
+    cout<< (5 | 3) <<endl;
+    cout<< ~(5) <<endl;
+    cout<< (5^5^6) <<endl;    //0^n=n
+    cout<< (5<<1) <<endl;    //0^n=n
+    cout<< (5>>1) <<endl;    //0^n=n
+
+    int n=10;
+    if(n&1){
+        cout<<"odd munber";
+    }
+    else{
+        cout<<"Even number";
+    }
+    return 0;
+}
